0a88e06b0267bb819195180d83f885b43adfd642
Gabriel Huitron <gabo.alejandro.huitron@gmail.com>
2019-10-04 22:56:10 -0500

    Cambios en suggestionDTO

--- src/suggestions/suggestion.dto.ts
+++ src/suggestions/suggestion.dto.ts
@@ -1,14 +1,26 @@
 import { IsString, IsInt } from 'class-validator';
 import { PsychologistRO } from '../psychologists/psychologist.dto';
+import { Column } from 'typeorm';
 
 export class SuggestionDTO {
   @IsString()
   content: string;
+
+  @IsString()
+  gender: string;
+
+  @IsString()
+  rangeAge: string;
+
+  @Column()
+  depressionType: string;
 }
 
 export class SuggestionRO {
   id: string;
   created: Date;
   content: string;
+  gender: string;
+  rangeAge: string;
   savedBy: PsychologistRO;
 }
