2b0ca7999d94945e18bf148b56b076c37b3d94d2
Yahir Trejo <yahir.trejo.g@gmail.com>
2019-11-05 15:21:30 -0600

    try .profile file for keys

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,17 +17,6 @@ import { ClassificationCriteriaService } from './classification-criteria/classif
 export class AppController {
   constructor(private readonly authService: AuthService, private ccs: ClassificationCriteriaService) {
     this.ccs.populate();
-    try {
-      if (process.env.NODE_ENV === 'production') {
-        console.log('writing file');
-        const fs = require('fs');
-        console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-        console.log(process.env.GOOGLE_CREDENTIALS);
-        fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, `/app/${process.env.GOOGLE_CREDENTIALS}`, (error) => console.log('error', error));
-      }
-    } catch (e) {
-      console.log('No GCP credentials');
-    }
   }
 
   @Post('auth/login')
