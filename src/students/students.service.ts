abf491253931af7da8660bf96ca4cc96731d1b74
Gabriel Huitron <gabo.alejandro.huitron@gmail.com>
2019-11-11 22:45:25 -0600

    Criterio fecha tdp funcionando, corrección de camir test

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -20,7 +20,7 @@ export class StudentsService {
       user: student.user ? student.user.toResponseObject() : null,
       posts: student.posts || null,
       diagnostics: student.diagnostics || null,
-      camirTest: student.camirTest || null,
+      camirTests: student.camirTests || null,
     };
 
     return responseObject;
