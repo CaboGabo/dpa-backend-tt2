667da1b1b43a3d449c2805930b7c2da6cb210d4f
cabogabo <gabo.alejandro.huitron@gmail.com>
2019-11-12 18:42:17 -0600

    Corrección ortográfica

--- src/classification-criteria/classification-criteria.data.ts
+++ src/classification-criteria/classification-criteria.data.ts
@@ -1,58 +1,60 @@
 export const criteria: any[] = [
-    {
-        name: "criteriaA2",
-        description : "Disminución acusada del interés o de la capacidad para el placer en todas o casi todas las actividades",
-        keyname: "A2"
-    },
-    {
-        name: "criteriaA3",
-        description : "Pérdida/aumento de peso/apetito",
-        keyname: "A3"
-    },
-    {
-        name: "criteriaA4",
-        description : "Insomnio",
-        keyname: "A4"
-    },
-    {
-        name: "criteriaA6",
-        description : "Fátiga o pérdida de energía",
-        keyname: "A6"
-    },
-    {
-        name: "criteriaA7",
-        description : "Sentimientos de inutilidad y culpa",
-        keyname: "A7"
-    },
-    {
-        name: "criteriaA8",
-        description : "Disminución de la capacidad para concentrarse o indecisión",
-        keyname: "A8"
-    },
-    {
-        name: "criteriaA9",
-        description : "Pensamientos recurrentes de muerte",
-        keyname: "A9"
-    },
-    {
-        name: "criteriaB1",
-        description : "Malestar clínicamente significativo o deterioro en lo social, laboral",
-        keyname: "B1"
-    },
-    {
-        name: "criteriaB4",
-        description : "Baja autoestima",
-        keyname: "B4"
-    },
-    {
-        name: "criteriaB6",
-        description : "Sentimientos de desesperanza",
-        keyname: "B6"
-    },
-    {
-        name: "criteriaC1",
-        description : "Efectos fisiológicos de una sustancia o de otra afección médica",
-        keyname: "C1"
-    },
-    
+  {
+    name: 'criteriaA2',
+    description:
+      'Disminución acusada del interés o de la capacidad para el placer en todas o casi todas las actividades',
+    keyname: 'A2',
+  },
+  {
+    name: 'criteriaA3',
+    description: 'Pérdida/aumento de peso/apetito',
+    keyname: 'A3',
+  },
+  {
+    name: 'criteriaA4',
+    description: 'Insomnio',
+    keyname: 'A4',
+  },
+  {
+    name: 'criteriaA6',
+    description: 'Fatiga o pérdida de energía',
+    keyname: 'A6',
+  },
+  {
+    name: 'criteriaA7',
+    description: 'Sentimientos de inutilidad y culpa',
+    keyname: 'A7',
+  },
+  {
+    name: 'criteriaA8',
+    description: 'Disminución de la capacidad para concentrarse o indecisión',
+    keyname: 'A8',
+  },
+  {
+    name: 'criteriaA9',
+    description: 'Pensamientos recurrentes de muerte',
+    keyname: 'A9',
+  },
+  {
+    name: 'criteriaB1',
+    description:
+      'Malestar clínicamente significativo o deterioro en lo social, laboral',
+    keyname: 'B1',
+  },
+  {
+    name: 'criteriaB4',
+    description: 'Baja autoestima',
+    keyname: 'B4',
+  },
+  {
+    name: 'criteriaB6',
+    description: 'Sentimientos de desesperanza',
+    keyname: 'B6',
+  },
+  {
+    name: 'criteriaC1',
+    description:
+      'Efectos fisiológicos de una sustancia o de otra afección médica',
+    keyname: 'C1',
+  },
 ];
